1c05078656c6d136fbf0e28098979329383e15e6
Andrii Kobzar <Andrii Kobzar>
2017-03-24 18:26:31 +0200

    modifying views

--- Task9/backend/poll_backend/poll_backend/views.py
+++ Task9/backend/poll_backend/poll_backend/views.py
@@ -4,9 +4,10 @@ from poll_backend.models import Category
 import json
 
 
+@csrf_exempt
 def get_categories(request):
     all_categories = Category.Category.objects.to_json()
-    return HttpResponse(json.dumps(all_categories),content_type="application/json")
+    return HttpResponse("[{id:1, name:'Category1'},{id:2,name:'Category2'}]", content_type="application/json")
 
 
 @csrf_exempt
@@ -15,4 +16,4 @@ def create_category(request):
     body = json.loads(body_unicode)
     new_category = Category.Category(name=body["name"])
     new_category.save()
-    return HttpResponse(json.dumps(body),content_type="application/json")
+    return HttpResponse(json.dumps(body), content_type="application/json")
