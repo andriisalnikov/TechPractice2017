a583365a81dcd5f13ed348c29a6372a3f094798e
Serafima <serafima.pal@gmail.com>
2017-03-24 18:41:44 +0200

    adding frontend for categories

---
+++ Task9/frontend/poll-frontend/src/app/categories/app.category.ts
@@ -0,0 +1,11 @@
+export class Category {
+    /**
+     *
+     */
+    id:Number;
+    name:String;
+    constructor(id:Number, name:String) {
+        this.id = id;
+        this.name = name;
+    }
+}
