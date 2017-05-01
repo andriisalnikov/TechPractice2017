da0a1d32236c9d3db84b36600ed35eb8353c1692
Kolya Dmitrenko <kolya_rev@mail.ru>
2017-05-01 18:54:25 +0300

    add test view

--- Task02/SSPLS/ssplsapp/views.py
+++ Task02/SSPLS/ssplsapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+def index(request):
+	return HttpResponse("hello")
