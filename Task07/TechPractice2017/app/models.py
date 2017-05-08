b25aa8bbe119a0caf25949fc919a18206f963b52
awerebear <beyond.mirror@gmail.com>
2017-05-08 19:24:03 +0300

    Update models.py

--- Task07/TechPractice2017/app/models.py
+++ Task07/TechPractice2017/app/models.py
@@ -5,3 +5,23 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+
+class USER(models.Model):
+    id = models.AutoField("ID", primary_key=True)
+    name = models.CharField("name", max_length=20)
+    login = models.CharField("nickname", max_length=20)
+    email = models.CharField("e-mail", max_length=50)
+    password = models.CharField("password", max_length=30)
+    city = models.CharField("city", max_length=30)
+    pass
+
+class EVENT(models.Model):
+    id = models.AutoField("ID", primary_key=True)
+    name = models.CharField("event name", max_length=50)
+    details = models.TextField()
+    place = models.CharField("where", max_length=40)
+    date = models.DateField("when")
+    participants = models.ManyToManyField(USER)
+    pass
+
+  
