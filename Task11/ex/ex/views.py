3adf74cc555d989169285e9b4c0927de8b6d9d0d
Darkcodi <trooper982@users.noreply.github.com>
2017-05-26 17:35:43 +0300

    Finally pull request (#27)
    * Added README.md
    
    * Initial commit
    
    * Removed foreign directories
    
    * Changed project's hierarchy and added models
    
    * Registered models in admin panel and fixed models representation
    
    * Added 2 new views (and html files)
    Set up static/media file sharing (via Django)
    
    * Added new test form (with checkboxes)
    
    * Index page template
    
    * Start doing registration page
    
    * Doing registration page
    
    * Do registration page(DB sync)
    
    * Updated DB model and minor improvements
    
    * Add logo. Fix /files/static directory. New project name VEX.

--- Task11/ex/ex/views.py
+++ Task11/ex/ex/views.py
@@ -1,16 +1,65 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
-# from .models import Post
+from .models import TheUser, FileSet, TheFile
 
 import datetime
+
+
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "It is now %s." % now
     return HttpResponse(html)
 
-def index(request):
-    return HttpResponse("Yahoo! It works :D")
 
-# def post_list(request):
-#     posts = Post.objects.order_by('published_date')
-#     return render(request, 'html/index.html', {posts})
+def index(request):
+    objects_list = FileSet.objects.order_by('key')
+    template = loader.get_template('index.html')
+    context = { 'objects_list': objects_list }
+    return HttpResponse(template.render(context, request))
+
+
+def object_view(request, obj_id):
+    my_object = FileSet.objects.filter(pk=obj_id)
+    files_list = TheFile.objects.filter(object_id=obj_id)
+    template = loader.get_template('object_view.html')
+    context = {
+        'my_object': my_object[0],
+        'files_list': files_list
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def testform(request):
+    array = { 1:'one', 2:'two', 3:'three' }
+    template = loader.get_template('testform.html')
+    context = {
+        'error_message': 'no errors still',
+        'headertext': 'Test Form',
+        'array': array
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def registration(request):
+    if request.method == 'POST':
+        template = loader.get_template('message.html')
+        nick_is_used = len(TheUser.objects.filter(nick=request.POST['nick'])) > 0
+        mail_is_used = len(TheUser.objects.filter(email=request.POST['email'])) > 0
+        messages = []
+        if nick_is_used:
+            messages.append('Sorry, Nick ' + request.POST['nick'] + ' is already in use.')
+        if mail_is_used:
+            messages.append('Sorry, E-mail ' + request.POST['email'] + ' is already in use.')
+        context = { 'messages': messages }
+        if (not nick_is_used) & (not mail_is_used):
+            u = TheUser(nick=request.POST['nick'], password_hash=request.POST['password'], email=request.POST['email'])
+            u.save()
+            context = {'messages': ['Thanks for registration! Confirmation link was sent to email ' + request.POST['email']]}
+        return HttpResponse(template.render(context, request))
+
+    else:
+        template = loader.get_template('registration.html')
+        context = {}
+        return HttpResponse(template.render(context, request))
+
--- Task11/ex/techpractice/urls.py
+++ Task11/ex/techpractice/urls.py
@@ -1,4 +1,4 @@
-"""ex URL Configuration
+"""techpractice URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from ex import views
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^testform/', views.testform, name='form1'),
     url(r'^$', views.index, name='index'),
     url(r'^registration/', views.registration, name='registration'),
+    url(r'^objects/(?P<obj_id>[0-9]+)/$', views.object_view, name='objects'), # ex: /objects/5/
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
