f115fb647e02e970dd45de8d1b0e1023fd7aed40
Kolya Dmitrenko <kolya_rev@mail.ru>
2017-06-05 13:13:14 +0300

    Creating game and adding it to database

--- Task02/SSPLS/ssplsapp/views.py
+++ Task02/SSPLS/ssplsapp/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.template.loader import get_template
 from django.template import Context
+from .forms import ContactForm
+from .models import Game, User
 def index(request):
 	context_data = ""
 	return render(request, 'index.html', dict(context_data))
@@ -26,3 +28,22 @@ def creategame(request):
 def game(request):
 	context_data = ""
 	return render(request, 'game-result.html', dict(context_data))
+def addgame(reguest):
+    context_data = ""
+    if reguest.method == 'POST':
+        form = ContactForm(reguest.POST)
+        # Если форма заполнена корректно, сохраняем все введённые пользователем значения
+        if form.is_valid():
+            bet = form.cleaned_data['bet']
+            #вставка в базу
+            u1 = User.objects.get(username='test1')
+            u2 = User.objects.get(username='np')
+            g = Game(firstuser=u1,seconduser=u2,firstbet=bet,secondbet='nb',status='np')
+            g.save()
+            return HttpResponse(bet)
+        else:
+            return HttpResponse('invalid')
+    else:
+        form = ContactForm()
+    #Выводим форму в шаблон
+    return HttpResponse('nopost')
