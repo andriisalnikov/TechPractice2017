5f27e54b8e2c7b1ee12df85a3ad115925b5796e1
Masha Wolfhound <mashawolfhound@gmail.com>
2017-06-11 16:29:06 +0300

    Final (#34)
    * So, Your turn, dearest teammates!
    
    This django chat is the working project You have to try ti run by yourself. Install Virtual Machine( Virtual Box, use Bridge adapter, in settings.py add your virtual machine ip address to ALLOWED_HOSTS, do following commands: 
    1 - Install python2.7: sudo apt-get install python2.7
    1.1. Commands python -V and pi -V have to show python2.7 version
    2 - Install pip: sudo apt-get install python-pip)
    3 - Upgrade pip: pip install --upgrade pip
    then go to project folder and install requiered pakages:
    4  pip install -r requirements.txt
    5. python manage.py makemigrations
    TO ADD SuperUser to access admin page : python manage.py createsuperuser
    6. python manage.py runserver <virtual machine ip>:8000 //turn off the firewall or add rule if any troubles
    
    192.168.0.106:8000/admin  THIS IP IS AN EXAMPLE - to create users and chats
    192.168.0.106:8000
    192.168.0.106:8000/chat - this is chat
    
    We have to add authentication by social neworks - it is made by https://simpleisbetterthanc
    
    * Виртуалки
    
    Ребята, Вам даже не нужно настраивать виртуалку. Вот ссылка https://drive.google.com/open?id=0Bx1jL41xGyYrNTFpRU5vN3B4TUk
    Готовая, просто создайте машину и подключите к ней этот диск.
    Дальше Вам нужно будет только поставить активировать виртуалэнв и и установить реквайроментс.ткст. Выполнить миграцию (возможно и не нужно будет).
    ВАЖНО!!!!
    Нам нужен в команду dba, что займется статистикой, делаем интергацию с соц сетями (Ира занимается) и интерфейс с выбором.
    
    * FinalCleanUp
    
    done --> clean unnes files
    
    * rem
    
    old files

--- Task08/chat_v2/channels_chat/routing.py
+++
@@ -1,9 +0,0 @@
-# In routing.py
-from channels import route
-from channels import include
-
-channel_routing = [
-    include("chat.routing.websocket_routing"),
-    include("chat.routing.custom_routing"),
-]
-
